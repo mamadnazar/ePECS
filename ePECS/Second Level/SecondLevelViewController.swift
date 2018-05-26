d7ecc1ae7b731c20a020dfa83447a79db99f6945
mamadnazar <shohkarim7@gmail.com>
2018-05-26 17:13:04 +0600

    second level initial stage

---
+++ ePECS/Second Level/SecondLevelViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondLevelViewController.swift
+//  ePECS
+//
+//  Created by Khasanza on 5/26/18.
+//  Copyright © 2018 neobis. All rights reserved.
+//
+
+import UIKit
+
+class SecondLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
