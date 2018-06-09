f2a7019f4750ebe0bef6ab9eb8b7ab512aa22b18
mamadnazar <shohkarim7@gmail.com>
2018-06-09 16:12:55 +0600

    bezier curve

--- ePECS/Supporting Files/CustomUI.swift
+++ ePECS/Supporting Files/CustomUI.swift
@@ -41,6 +41,33 @@ class CustomView: UIView {
     }
 }
 
+class Curve: UIView {
+    
+    var path: UIBezierPath!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+
+    override func draw(_ rect: CGRect) {
+        
+        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2), radius: self.frame.size.height/2, startAngle: CGFloat(4.7), endAngle: CGFloat(1.5), clockwise: true)
+        
+        UIColor.red.setStroke()
+        path.stroke()
+        
+        
+    }
+    
+}
+
 //class CustomButton: UIButton {
 //
 //    override init(frame: CGRect) {
