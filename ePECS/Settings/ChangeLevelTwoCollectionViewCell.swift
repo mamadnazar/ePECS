bdab3859de19b063e7a115af866e7a6c322708d8
mamadnazar <shohkarim7@gmail.com>
2018-06-20 09:35:11 +0600

    settings design updated deleting feature added

--- ePECS/Settings/ChangeLevelTwoCollectionViewCell.swift
+++ ePECS/Settings/ChangeLevelTwoCollectionViewCell.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
+protocol ChangeLevelTwoCollectionViewCellDelegate {
+    
+     func didTapDelete(cardToDelete: Card)
+}
+
 class ChangeLevelTwoCollectionViewCell: UICollectionViewCell {
  
+    @IBOutlet weak var changeLevelTwoCustomView: CustomView! {
+        didSet {
+            self.changeLevelTwoCustomView.layer.cornerRadius = 20
+        }
+    }
     @IBOutlet weak var changeLevelTwoImageView: UIImageView!
+    var card: Card!
+    var delegate: ChangeLevelTwoCollectionViewCellDelegate?
     
+    func setPhraseCard(card: Card) {
+        self.card = card
+        changeLevelTwoImageView.image = card.image
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+        delegate?.didTapDelete(cardToDelete: card)
+    }
 }
--- ePECS/Settings/ChangeLevelTwoViewController.swift
+++ ePECS/Settings/ChangeLevelTwoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ChangeLevelTwoViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class ChangeLevelTwoViewController: UIViewController {
     
     class var shared: ChangeLevelTwoViewController {
         struct Static {
@@ -19,6 +19,7 @@ class ChangeLevelTwoViewController: UIViewController, UICollectionViewDataSource
     
     private var cards_images = [#imageLiteral(resourceName: "meGirl"), #imageLiteral(resourceName: "хочу"), #imageLiteral(resourceName: "да"), #imageLiteral(resourceName: "нет"), #imageLiteral(resourceName: "мяч"), #imageLiteral(resourceName: "car"), #imageLiteral(resourceName: "кушать"), #imageLiteral(resourceName: "пить"), #imageLiteral(resourceName: "помоги"), #imageLiteral(resourceName: "спать")]
     private var cards_names = ["Я", "Хочу", "Да", "Нет", "Мяч", "Машина", "Кушать", "Пить", "Помоги", "Спать"]
+    private var cards: [Card] = []
     
     var selectedCard: Int?
     var cellCardCount = 0
@@ -27,7 +28,18 @@ class ChangeLevelTwoViewController: UIViewController, UICollectionViewDataSource
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupCards()
+    }
+    
+    private func setupCards() {
+        var card: Card?
+        var index: Int?
+        for i in cards_names {
+            index = cards_names.index(of: i)
+            card = Card(index: index!, name: cards_names[index!], image: cards_images[index!])
+            cards.append(card!)
+        }
     }
        
     func getCardsImages() -> [UIImage] {
@@ -37,33 +49,37 @@ class ChangeLevelTwoViewController: UIViewController, UICollectionViewDataSource
     func getCardsNames() -> [String] {
         return cards_names
     }
-    
-    @IBAction func deleteCard(_ sender: Any) {
-//        if (selectedCard! >= 0) {
-//            cards_images.remove(at: selectedCard!)
-//            changeLevelTwoCollectionView.reloadData()
-//        }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension ChangeLevelTwoViewController: ChangeLevelTwoCollectionViewCellDelegate {
+    func didTapDelete(cardToDelete: Card) {
+        cards.remove(at: cards.index(of: cardToDelete)!)
+        changeLevelTwoCollectionView.reloadData()
     }
     
+}
+
+extension ChangeLevelTwoViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cards_images.count
+        return cards.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChangeLevelTwoCollectionViewCell", for: indexPath) as! ChangeLevelTwoCollectionViewCell
-        cell.changeLevelTwoImageView.image = cards_images[indexPath.row]
+        cell.setPhraseCard(card: cards[indexPath.row])
+        cell.delegate = self
         return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        cards_images.remove(at: indexPath.row)
-        cards_names.remove(at: indexPath.row)
-        changeLevelTwoCollectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        let width = UIScreen.main.bounds.width / 3 - 8
+        let width = changeLevelTwoCollectionView.frame.size.width / 2
         return CGSize(width: width, height: width)
     }
     
@@ -75,9 +91,5 @@ class ChangeLevelTwoViewController: UIViewController, UICollectionViewDataSource
         return 0
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-   
 }
+
